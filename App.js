7883e81e0fc4b1eb90969de880c3f462e37c3686
net-c215 <williamspino42@gmail.com>
2021-09-25 00:36:44 +0530

    added react native elements

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, ThemeProvider } from 'react-native-elements';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <Button title="Hey!"  raised/>
+      </View>
+    </ThemeProvider>
+
   );
 }
 
