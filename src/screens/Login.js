01ca1b9a2a24b034c9b54cdbce4814f3c2eb9f2f
net-c215 <williamspino42@gmail.com>
2021-09-25 22:50:53 +0530

    login screen fixed

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -20,7 +20,7 @@ export default function Login({ navigation }) {
     const handleLogin = async () => {
         console.log(credentials)
         try {
-            await Firebase.auth().signInWithEmailAndPassword(credentials.email,credentials.password)
+            await Firebase.auth().signInWithEmailAndPassword(credentials.email, credentials.password)
                 .then((res) => {
                     console.log(res)
                 })
@@ -38,41 +38,41 @@ export default function Login({ navigation }) {
     }, [])
 
     return (
-        <>
-            <ImageBackground source={{ uri: 'https://source.unsplash.com/WLUHO9A_xik/1600x900' }} resizeMode="cover" style={tw`h-full opacity-90`} />
+        <View style={tw`h-full`}>
 
-            <View style={tw`h-2/4 absolute bg-black  w-5/6 mx-auto flex    opacity-90 top-1/4 px-5 shadow-2xl rounded-xl `}>
+            <View style={tw`h-2/4 w-5/6 mx-auto  top-1/4 px-3   `}>
 
-                <Text style={tw`text-center text-black text-4xl py-3 text-blue-400 font-bold  `} >Login</Text>
+                <Text style={tw` text-black text-4xl py-3 text-blue-900 font-bold  `} >Login</Text>
 
-                <View style={tw`my-2`} >
+                <View style={tw`my-2 mt-5`} >
                     <Input
-                        placeholder='Enter email'
-                        leftIcon={{ type: 'font-awesome-5', name: 'envelope', color: 'white', marginRight: 10, size: 20 }}
-                        style={tw`text-sm text-white`}
+                        placeholder='Email ID'
+                        leftIcon={{ type: 'font-awesome-5', name: 'at', color: 'gray', marginRight: 7, size: 20 }}
+                        style={tw`text-base  border-0 `}
                         onChangeText={value => setCredentials({ ...credentials, email: value })}
                     />
                 </View>
 
-                <View style={tw`my-1`} >
+                <View style={tw` mb-7`} >
                     <Input
-                        placeholder='Enter password'
+                        placeholder='Password'
                         leftIcon={{
-                            type: 'font-awesome-5', name: 'key', color: 'white',
-                            marginRight: 10, size: 20
+                            type: 'font-awesome-5', name: 'lock', color: 'gray',
+                            marginRight: 7, size: 20
                         }}
+                        secureTextEntry={true}
                         value={credentials.password}
-                        style={tw`text-sm text-white `}
+                        style={tw`text-base `}
                         onChangeText={value => setCredentials({ ...credentials, password: value })}
                     />
                 </View>
 
-                <Text onPress={() => console.log("Forget password")} style={tw`my-3 text-center text-blue-400`}>Forget Password?</Text>
+                <Text onPress={() => console.log("Forget password")} style={tw`absolute top-44 right-4 text-blue-600`}>Forget?</Text>
 
                 <Button
                     title="Login"
                     onPress={() => handleLogin()}
-                    buttonStyle={tw`my-2`}
+                    buttonStyle={tw`my-2 py-3 rounded-xl bg-blue-600`}
                 />
                 {/* <Button
                     title="Sign In "
@@ -87,8 +87,11 @@ export default function Login({ navigation }) {
                     iconRight={true}
                     onPress={() => handleLogin()}
                 /> */}
-
+                <Text style={tw`text-center text-gray-500 mt-4`} >New to Elctroflux?
+                    <Text onPress={() => navigation.navigate("SignupScreen")} style={tw`text-blue-500`} > Register</Text>
+                </Text>
             </View>
-        </>
+
+        </View>
     )
 }
