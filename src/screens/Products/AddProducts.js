2d53f2cb2c98e180a237e2b27266f7fe01b6d0ee
net-c215 <williamspino42@gmail.com>
2021-10-12 23:19:23 +0530

    product  screen add functionality added

--- src/screens/Products/AddProducts.js
+++ src/screens/Products/AddProducts.js
@@ -1,4 +1,5 @@
 import React, { useLayoutEffect, useState } from "react";
+import { Alert } from "react-native";
 import { View, Text } from "react-native";
 import { Icon } from "react-native-elements";
 import { TouchableOpacity } from "react-native-gesture-handler";
@@ -6,16 +7,16 @@ import { Button, TextInput } from "react-native-paper";
 import tw from "tailwind-react-native-classnames";
 
 export default function AddProduct({ navigation }) {
-    const [state, setState] = React.useState({ open: false });
     const [data, setData] = useState({
         name: "",
         price: "",
         mrp: "",
         description: "",
     });
-    const onStateChange = ({ open }) => setState({ open });
 
-    const { open } = state;
+    const handleAdd = () => {
+        return Alert(data.name);
+    };
     useLayoutEffect(() => {
         navigation.setOptions({
             title: "Add Product",
@@ -36,23 +37,7 @@ export default function AddProduct({ navigation }) {
             ),
         });
     }, [navigation]);
-    const renderItem = ({ item }) => (
-        <TouchableOpacity>
-            <List.Item
-                title={item.name}
-                description={item.description}
-                left={(props) => <List.Icon {...props} icon="folder" />}
-                right={() => (
-                    <View>
-                        <Text style={tw`font-bold text-lg text-right`}>
-                            ₹{item.price}
-                        </Text>
-                        <Text>Mrp: ₹{item.mrp}</Text>
-                    </View>
-                )}
-            />
-        </TouchableOpacity>
-    );
+
     return (
         <View style={tw`h-full bg-blue-50`}>
             <View style={tw`h-full flex-1 p-4`}>
@@ -84,7 +69,7 @@ export default function AddProduct({ navigation }) {
                 />
 
                 <Button
-                    onPress={() => handleLogin()}
+                    onPress={() => handleAdd()}
                     // loading={isPending}
                     contentStyle={tw` py-2  rounded-xl `}
                     style={tw`rounded-xl `}
