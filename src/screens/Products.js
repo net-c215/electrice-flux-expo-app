e615f1ba057f2f1aa36e8f067d588d201e2e189e
net-c215 <williamspino42@gmail.com>
2021-10-12 21:40:31 +0530

    product  screen header created

--- src/screens/Products.js
+++ src/screens/Products.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import { View, Text } from 'react-native'
+import { Icon } from 'react-native-elements';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import tw from 'tailwind-react-native-classnames'
 
 
-export default function Products() {
+export default function Products({navigation}) {
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: "Products",
+            headerStyle: {
+                backgroundColor: "darkgreen",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+                fontWeight: "bold",
+            },
+            headerRight: () => (
+                <TouchableOpacity
+                    onPress={() => alert("Hello Raju")}
+                    style={{ marginRight: 10 }}
+                >
+                    <Icon name="user-alt" type="font-awesome-5" color="white" />
+                </TouchableOpacity>
+            ),
+        });
+    }, [navigation]);
     return (
         <View style={tw`h-full bg-blue-50`}> 
             <View style={tw`h-full`}>
