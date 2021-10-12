b7816b45b203a36fc5f1949f77f231ba0b98142b
net-c215 <williamspino42@gmail.com>
2021-10-12 22:31:38 +0530

    product  screen ui

--- src/screens/Products.js
+++ src/screens/Products.js
@@ -1,7 +1,7 @@
 import React, { useLayoutEffect } from "react";
 import { View, Text } from "react-native";
 import { Icon } from "react-native-elements";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
 import { FAB, IconButton, List, Portal } from "react-native-paper";
 import tw from "tailwind-react-native-classnames";
 
@@ -31,13 +31,30 @@ export default function Products({ navigation }) {
             ),
         });
     }, [navigation]);
+    const renderItem = ({ item }) => (
+        <TouchableOpacity>
+            <List.Item
+                title={item.name}
+                description={item.description}
+                left={(props) => <List.Icon {...props} icon="folder" />}
+                right={() => (
+                    <View>
+                        <Text style={tw`font-bold text-lg text-right`}>
+                            ₹{item.price}
+                        </Text>
+                        <Text>Mrp: ₹{item.mrp}</Text>
+                    </View>
+                )}
+            />
+        </TouchableOpacity>
+    );
     return (
         <View style={tw`h-full bg-blue-50`}>
             <View style={tw`h-full flex-1`}>
-                <List.Item
-                    title="First Item"
-                    description="Item description"
-                    left={(props) => <List.Icon {...props} icon="folder" />}
+                <FlatList
+                    data={tempData}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.id}
                 />
 
                 <FAB
@@ -60,48 +77,112 @@ const tempData = [
     {
         id: 1,
         name: "PVC Pipe",
+        description: "lol",
         price: 340,
         mrp: 120,
     },
     {
         id: 2,
         name: "Tafflon",
+        description: "lol",
         price: 50,
         mrp: 10,
     },
     {
         id: 4,
-        name: "PVC Pipe",
+        name: "40 mm",
+        description: "lol",
         price: 340,
         mrp: 120,
     },
     {
-        id: 1,
-        name: "PVC Pipe",
+        id: 11,
+        name: "6 mm",
+        description: "lol",
         price: 340,
         mrp: 120,
     },
     {
-        id: 1,
-        name: "PVC Pipe",
+        id: 123,
+        name: "18 mm",
+        description: "lol",
         price: 340,
         mrp: 120,
     },
     {
-        id: 1,
-        name: "PVC Pipe",
+        id: 451,
+        name: "6 mm A",
+        description: "lol",
         price: 340,
         mrp: 120,
     },
     {
-        id: 1,
-        name: "PVC Pipe",
+        id: 15,
+        name: "6 mm B",
+        description: "lol",
         price: 340,
         mrp: 120,
     },
     {
-        id: 1,
+        id: 2341,
+        name: "Jeera",
+        description: "lol",
+        price: 340,
+        mrp: 120,
+    },
+    {
+        id: 11312,
         name: "PVC Pipe",
+        description: "lol",
+        price: 340,
+        mrp: 120,
+    },
+    {
+        id: 124242,
+        name: "Tafflon",
+        description: "lol",
+        price: 50,
+        mrp: 10,
+    },
+    {
+        id: 4323423,
+        name: "40 mm",
+        description: "lol",
+        price: 340,
+        mrp: 120,
+    },
+    {
+        id: 112341,
+        name: "6 mm",
+        description: "lol",
+        price: 340,
+        mrp: 120,
+    },
+    {
+        id: 12123412343,
+        name: "18 mm",
+        description: "lol",
+        price: 340,
+        mrp: 120,
+    },
+    {
+        id: 4512341,
+        name: "6 mm A",
+        description: "lol",
+        price: 340,
+        mrp: 120,
+    },
+    {
+        id: 1345,
+        name: "6 mm B",
+        description: "lol",
+        price: 340,
+        mrp: 120,
+    },
+    {
+        id: 234341,
+        name: "Jeera",
+        description: "lol",
         price: 340,
         mrp: 120,
     },
