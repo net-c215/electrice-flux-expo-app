a6f4d576be4399a4eb9febb0da6a1c6d73e85f51
net-c215 <williamspino42@gmail.com>
2021-10-23 23:49:44 +0530

    bill preview screen added

---
+++ src/screens/BillPreview/index.js
@@ -0,0 +1,35 @@
+import React, { useLayoutEffect } from "react";
+import { View, Text } from "react-native";
+import { Icon } from "react-native-elements";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+export default function BillPreviewScreen({navigation}) {
+    
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: "Bill Preview",
+            headerStyle: {
+                backgroundColor: "#2E8BC0",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+                fontWeight: "bold",
+            },
+            headerRight: () => (
+                <TouchableOpacity
+                    onPress={() => navigation.push("HomeScreen")}
+                    style={{ marginRight: 10 }}
+                >
+                    <Icon name="user-alt" type="font-awesome-5" color="white" />
+                </TouchableOpacity>
+            ),
+        });
+    }, [navigation]);
+
+
+    return (
+        <View>
+            <Text>BillPreviewScreen</Text>
+        </View>
+    );
+}
